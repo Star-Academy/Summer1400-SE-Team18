19cb4de66996bc58cd047e9d0553c31215141900
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-07-24 13:24:13 +0430

    added view

---
+++ search1/src/main/java/model/Location.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Location {
+    int index;
+    public Location(int index) {
+        this.index = index;
+    }
+}
---
+++ search1/src/main/java/view/MainMenu.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.util.Scanner;
+
+public class MainMenu {
+    public static void run() {
+        String command;
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            command = scanner.nextLine();
+            if (command.startsWith("read")) {
+                read();
+            } else if (command.startsWith("search")) {
+                search();
+            } else if (command.startsWith("help")) {
+                help();
+            } else if (command.startsWith("quit")) {
+                System.exit(0);
+            }
+        }
+    }
+
+    private static void help() {
+        System.out.println("read : to add a file to database (with path)\n" + 
+                "search : search for a word in the current database\n" + 
+                "");
+    }
+}
