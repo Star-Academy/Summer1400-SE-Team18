165bf063ce4a0419ef12ba71f303f9625f038547
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-07-24 13:29:43 +0430

    Create MainMenuController.java

---
+++ search1/src/main/java/controller/MainMenuController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import model.Location;
+
+public class MainMenuController {
+    private final static HashMap<String, ArrayList<Location>> datas = new HashMap<>();
+}
