19b8782466f0b8e412338818e4aa454e07e35d15
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-07-24 13:26:25 +0430

    Update MainMenu.java

--- search1/src/main/java/view/MainMenu.java
+++ search1/src/main/java/view/MainMenu.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.util.Scanner;
+import java.util.function.ToDoubleBiFunction;
 
 public class MainMenu {
     public static void run() {
@@ -23,6 +24,14 @@ public class MainMenu {
     private static void help() {
         System.out.println("read : to add a file to database (with path)\n" + 
                 "search : search for a word in the current database\n" + 
-                "");
+                "quit : quit the program");
+    }
+
+    private static void search() {
+        // todo
+    }
+
+    private static void read() {
+        // todo
     }
 }
