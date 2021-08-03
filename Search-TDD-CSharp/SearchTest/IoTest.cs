6dc65e5485c56f45bdc7f78c64d5243b94e9ff7a
Mohammad Moshtaghi <mhmmoshtaghi@gmail.com>
2021-08-03 18:17:54 +0430

    Create IoTest.cs

---
+++ Search-TDD-CSharp/SearchTest/IoTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Search.IO;
+using Xunit;
+
+namespace SearchTest
+{
+    public class IoTest
+    {
+        private IReader _fileReader;
+        private IReader _folderReader;
+        
+        [Fact]
+        public void ReadFileTest()
+        {
+            var readingString = _fileReader.Read("TestDataBase/3");
+            var expectedString = "man sag mikham\nsag khoshgel - mikham !!! mio !!!\n";
+        }
+
+        [Fact]
+        public void ReadFolderTest()
+        {
+            
+        }
+    }
+}
