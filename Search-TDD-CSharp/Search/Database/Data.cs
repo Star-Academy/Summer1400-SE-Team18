065d1fc98ea9e777ae462cc2d5de673f64ca008c
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-08-03 17:07:39 +0430

    Update Data.cs

--- Search-TDD-CSharp/Search/Database/Data.cs
+++ Search-TDD-CSharp/Search/Database/Data.cs
@@ -1,6 +1,16 @@
-﻿namespace Search.Database
+﻿using System.Collections.Generic;
+
+namespace Search.Database
 {
     public class Data
     {
+        public string Word { get; }
+        public List<string> FilesWithWordInThem { get; }
+
+        public Data(string word, List<string> filesWithWordInThem)
+        {
+            this.Word = word;
+            this.FilesWithWordInThem = filesWithWordInThem;
+        }
     }
 }
