fa6cd8f16034cfca9f12a19e24a4da863b191006
Mohammad Moshtaghi <mhmmoshtaghi@gmail.com>
2021-08-03 11:36:38 +0430

    create phase4 solution

---
+++ school/School/Scores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
