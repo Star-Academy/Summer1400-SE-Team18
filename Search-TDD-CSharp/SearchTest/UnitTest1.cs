2fc30c0c5a89dd4d0067b809e9cac2e80861173e
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-08-03 16:24:26 +0430

    Created Project
    .gitignore also updated

---
+++ Search-TDD-CSharp/SearchTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SearchTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("abbas", "abbas");
+        }
+    }
+}
