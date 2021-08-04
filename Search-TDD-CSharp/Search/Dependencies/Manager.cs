bd545008b58737164fa50aa13dbcf2ee412ba570
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-08-04 15:01:24 +0430

    Update Manager.cs

--- Search-TDD-CSharp/Search/Dependencies/Manager.cs
+++ Search-TDD-CSharp/Search/Dependencies/Manager.cs
@@ -5,13 +5,12 @@ using Search.Word;
 
 namespace Search.Dependencies
 {
-    public class Manager
+    public abstract class Manager
     {
         public static IReader FileReaderInstance = new FileReader();
         public static IReader FolderReaderInstance = new FolderReader();
         public static EnglishStemmer Stemmer = new EnglishStemmer();
         public static IWordProcessor WordProcessorInstance = new WordProcessor();
-        public static IDatabase Database = new Database(); 
-
+        public static IDatabase Database = new Database();
     }
 }
