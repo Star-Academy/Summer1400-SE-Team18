27c3166148b31016557657205694e9af0ddaccf7
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-08-04 12:16:46 +0430

    word processor test

--- Search-TDD-CSharp/SearchTest/WordProcessorTest.cs
+++ Search-TDD-CSharp/SearchTest/WordProcessorTest.cs
@@ -1,9 +1,53 @@
-﻿using Xunit;
+﻿using NSubstitute;
+using Xunit;
+using Search.WordProcessor;
 
 namespace SearchTest
 {
     public class WordProcessorTest
     {
-        
+        private IWordProcessor _wordProcessor;
+
+        [Fact]
+        public void Should_Parse_Text()
+        {
+            var expected = new string[]
+            {
+                "Detract" ,
+                "yet" ,
+                "delight" ,
+                "written" ,
+                "farther" ,
+                "his" ,
+                "general" ,
+                "If" ,
+                "in" ,
+                "so" ,
+                "bred" ,
+                "at" ,
+                "da" ,
+                "re" ,
+                "rose" ,
+                "lose" ,
+                "go" ,
+                "od" ,
+                "Feel" ,
+                "and" ,
+                "make" ,
+                "two" ,
+                "real" ,
+                "miss" ,
+                "use" ,
+                "ea" ,
+                "sy" ,
+                "Celebrated" ,
+                "delightful" ,
+                "an"
+            };
+            var text = "Detract111222333 yet delight written farther11111 his general2222. If in so bred" +
+                       " at da##*()(((((()))re rose lose go444od. Feel and make two real miss use ea1233123sy. Celebrated delightful an";
+            var parsedText = _wordProcessor.ParseText(text);
+            Assert.Equal(expected, parsedText);
+        }
     }
 }
