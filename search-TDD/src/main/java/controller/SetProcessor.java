a267d093501999a29ce5194142e5c47f26a9220c
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-08-06 18:23:36 +0430

    Update SetProcessor.java

--- search-TDD/src/main/java/controller/SetProcessor.java
+++ search-TDD/src/main/java/controller/SetProcessor.java
@@ -4,15 +4,15 @@ import java.util.Set;
 
 public class SetProcessor {
     
-    public void addAll(Set initialSet, Set secondSet) {
+    public void addAll(Set<String> initialSet, Set<String> secondSet) {
         initialSet.addAll(secondSet);
     }
 
-    public void removeAll(Set initialSet, Set secondSet) {
+    public void removeAll(Set<String> initialSet, Set<String> secondSet) {
         initialSet.removeAll(secondSet);
     }
 
-    public void retainAll(Set initialSet, Set secondSet) {
+    public void retainAll(Set<String> initialSet, Set<String> secondSet) {
         initialSet.retainAll(secondSet);
     }
 }
