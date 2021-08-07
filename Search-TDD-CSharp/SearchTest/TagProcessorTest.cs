6dd27b272793a1f51e7dd45807f57385d788b167
Mohammad Moshtaghi <mhmmoshtaghi@gmail.com>
2021-08-07 13:36:14 +0430

    Update TagProcessorTest.cs

--- Search-TDD-CSharp/SearchTest/TagProcessorTest.cs
+++ Search-TDD-CSharp/SearchTest/TagProcessorTest.cs
@@ -17,17 +17,22 @@ namespace SearchTest
         [InlineData("+mohammad", TagType.Plus)]
         [InlineData("-ali", TagType.Minus)]
         [InlineData("reza", TagType.NoTag)]
-        public void Tag_ShouldProcessorCorrectly_ForAllTagTypes(string word, TagType tagType)
+        public void Tag_ShouldProcessTagTypesCorrectly_ForAllTagTypes(string command, TagType tagType)
         {
-            var expectedAndActualPair = new Dictionary<Tag, Tag>()
-            {
-                {new Tag("mohammad", TagType.Plus), _managerInstance.TagProcessor.Process("+mohammad")},
-                {new Tag("ali", TagType.Minus), _managerInstance.TagProcessor.Process("-ali")},
-                {new Tag("reza", TagType.NoTag), _managerInstance.TagProcessor.Process("reza")}
-            };
             var tagProcessor = _managerInstance.TagProcessor;
-            var tagged = tagProcessor.Process(word);
+            var tagged = tagProcessor.Process(command);
             Assert.Equal(tagged.Type, tagType);
+        }
+
+        [Theory]
+        [InlineData("+mohammad", "mohammad")]
+        [InlineData("-ali", "ali")]
+        [InlineData("reza", "reza")]
+        public void Tag_ShouldProcessWordsCorrectly_ForAllTagTypes(string command, string word)
+        {
+            var tagProcessor = _managerInstance.TagProcessor;
+            var tagged = tagProcessor.Process(command);
+            Assert.Equal(tagged.Word, word);
         }
     }
 }
