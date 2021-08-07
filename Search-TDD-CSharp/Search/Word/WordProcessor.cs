51de54e13b21405552f9d8004e3d879047cc9cba
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-08-07 12:07:35 +0430

    Update WordProcessor.cs

--- Search-TDD-CSharp/Search/Word/WordProcessor.cs
+++ Search-TDD-CSharp/Search/Word/WordProcessor.cs
@@ -6,8 +6,9 @@ namespace Search.Word
 {
     public class WordProcessor : IWordProcessor
     {
-        
         private readonly Manager _managerInstance = Manager.GetInstance();
+        private const string NonAlphabeticPattern = "[^A-Za-z ]";
+        private const string SpaceRegex = "\\s+";
 
         public string[] ParseText(string text)
         {
@@ -26,12 +27,12 @@ namespace Search.Word
 
         public string[] SplitWordsInText(string text)
         {
-            return Regex.Split(text, "\\s+");
+            return Regex.Split(text, SpaceRegex);
         }
 
         public string RemoveNonAlphabeticalWords(string text)
-        { 
-            text = Regex.Replace(text, "[^A-Za-z ]", " ");
+        {
+            text = Regex.Replace(text, NonAlphabeticPattern, " ");
             return text.Trim();
         }
     }
