dec8a6245c93dc5bc070672292ade045f26d5ef7
Mohammad Moshtaghi <mhmmoshtaghi@gmail.com>
2021-08-08 10:52:38 +0430

    Update IOTest.cs

--- Search-TDD-CSharp/SearchTest/IOTest.cs
+++ Search-TDD-CSharp/SearchTest/IOTest.cs
@@ -32,7 +32,7 @@ namespace SearchTest
             _fileContents[0] = KeyValuePair.Create("1",
                 $"Hello Dear,{_lineSeparator}I am Mohammad.{_lineSeparator}");
             _fileContents[1] = KeyValuePair.Create("3",
-                $"man sag mikham{_lineSeparator}sag khoshgel -  !!! mio !!!{_lineSeparator}");
+                $"man sag mikham{_lineSeparator}sag khoshgel - mikham !!! mio !!!{_lineSeparator}");
             _fileContents[2] = KeyValuePair.Create("4",
                 "Mir rafte dubai vase nakhle talaii !!");
         }
@@ -46,8 +46,8 @@ namespace SearchTest
         public void Read_ShouldReadCorrectly_WhenPathIsFile()
         {
             var readingData = _fileReader.Read("TestDataBase/3");
-            var expectedString = _fileContents[2].Value;
-            Assert.Equal(expectedString, readingData[_fileContents[2].Key]);
+            var expectedString = _fileContents[1].Value;
+            Assert.Equal(expectedString, readingData[_fileContents[1].Key]);
         }
 
         [Fact]
